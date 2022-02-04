1568df350b6af0db598bb3b1cf54e1e58a146877
CobaltoIV <ricas1998@gmail.com>
2022-02-04 08:30:39 +0000

    04-02-2022 Updates

--- Recording/demo.py
+++ Recording/demo.py
@@ -21,40 +21,40 @@ class RecordingSetup:
 
         for r in range(6):
             self.mainframe.rowconfigure(r, weight=1)
-        for c in range(3):
+        for c in range(2):
             self.mainframe.columnconfigure(c, weight=1)
 
        
         # Doctor Id entry
         self.id = StringVar()
         id_entry = ttk.Entry(self.mainframe, width=7, textvariable=self.id)
-        id_entry.grid(column=2, row=1, sticky=(W, E))
-        ttk.Label(self.mainframe, text="Doctor Id").grid(column=1, row=1)
+        id_entry.grid(column=1, row=1, sticky=(W, E))
+        ttk.Label(self.mainframe, text="Doctor Id").grid(column=0, row=1)
 
         # Error Label for Button
         self.error = StringVar()
-        ttk.Label(self.mainframe, textvariable=self.error).grid(column=3, row=1)
+        ttk.Label(self.mainframe, textvariable=self.error).grid(column=2, row=1)
 
         self.spec = StringVar()
         spec_entry = ttk.Combobox(self.mainframe, textvariable=self.spec)
-        spec_entry.grid(column=2, row=2, sticky=(W, E))
+        spec_entry.grid(column=1, row=2, sticky=(W, E))
         spec_entry['values'] = ('Neurologia', 'MedInt', 'MedGeral', 'Genecologia', 'Endocrinologia')
         spec_entry.state(["readonly"])
-        ttk.Label(self.mainframe, text="Doctor Specialty").grid(column=1, row=2)
+        ttk.Label(self.mainframe, text="Doctor Specialty").grid(column=0, row=2)
 
         
         
         self.mode =  StringVar()
         mode_entry_presential = ttk.Radiobutton(self.mainframe, text="Presential", variable=self.mode, value="Presential")
-        mode_entry_presential.grid(column=2)
+        mode_entry_presential.grid(column=1)
         mode_entry_virtual = ttk.Radiobutton(self.mainframe, text="Virtual", variable=self.mode, value="Virtual")
-        mode_entry_virtual.grid(column=2)
-        ttk.Label(self.mainframe, text="Type Of Consultation").grid(column=1, row=3)
+        mode_entry_virtual.grid(column=1)
+        ttk.Label(self.mainframe, text="Type Of Consultation").grid(column=0, row=3)
 
         #Record Button
         self.btn = ttk.Button(self.mainframe, text="Record", command=self.record)
         self.btn.grid(
-            column=2,sticky=(W, E, N, S))
+            column=1,sticky=(W, E, N, S))
         
 
         for child in self.mainframe.winfo_children():
@@ -62,13 +62,13 @@ class RecordingSetup:
 
         self.recNotice = ttk.Label(
             self.mainframe, text="Recording", foreground='red')
-        self.recNotice.grid(column=1, row=5)
+        self.recNotice.grid(column=0, row=5)
         self.recNotice.grid_remove()
         
     def record(self, *args):
         self.btn.configure(text="Stop")
         self.btn.configure(command=self.stop)
-        self.recNotice.grid(column=1, row=5)
+        self.recNotice.grid(column=0, row=5)
 
         
         
@@ -109,6 +109,10 @@ class RecordingSetup:
         video_location = f'{day_dir}/{name}'  
         # Command : ffmpeg -f video4linux2 -s hd720 -r 15 -input_format mjpeg -i /dev/video0 out.mp4
         self.p = subprocess.Popen(["ffmpeg", "-f", "video4linux2", "-s","hd720","-input_format","mjpeg", "-i", "/dev/video0", "-r", "15", video_location])
+        """
+        self.p = subprocess.Popen(["ffmpeg", "-f", "video4linux2", "-s","hd720","-input_format","mjpeg", "-i", "/dev/video0", "-r", "15","-preset","veryfast", video_location])
+        """
+       
        
 
     def stop(self, *args):
