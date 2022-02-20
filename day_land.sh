48e28f91258c7a218ceb01614d4f094e4a5b3e13
CobaltoIV <ricas1998@gmail.com>
2022-02-20 11:03:16 +0000

    Add docto classification correction

--- day_land.sh
+++ day_land.sh
@@ -1,7 +1,6 @@
-bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Endocrinologia/D3/Virtual/09-02-2022/ 
-bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Genecologia/D6/Virtual/11-02-2022/ 
-bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D5/Virtual/10-02-2022/ 
-bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Neurologia/D1/Virtual/11-02-2022/ 
-bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Neurologia/D2/Virtual/11-02-2022/ 
+#bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Endocrinologia/D10/Virtual/15-02-2022/ 
+bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Genecologia/D9/Virtual/15-02-2022/ 
+#bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Neurologia/D1/Presential/15-02-2022/ 
+
 
 
