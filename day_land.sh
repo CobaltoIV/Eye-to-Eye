ad4657f7be3baddfa0d8d4bc3ee3699979aeef98
CobaltoIV <ricas1998@gmail.com>
2022-04-21 14:10:02 +0100

    Final Presential week

--- day_land.sh
+++ day_land.sh
@@ -3,7 +3,7 @@
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Endocrinologia/D10/Virtual/05-04-2022 
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Endocrinologia/D10/Virtual/25-03-2022 
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Genecologia/D6/Presential/06-04-2022
-bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Genecologia/D9/Presential/14-04-2022
+bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Genecologia/D12/Presential/19-04-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Genecologia/D14/Presential/12-04-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Genecologia/D14/Virtual/08-04-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Genecologia/D14/Virtual/01-04-2022
@@ -16,9 +16,9 @@ bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/Genecologia/
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/29-03-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/07-04-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/15-03-2022
-#bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/23-03-2022
+bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/14-04-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D5/Virtual/03-03-2022
-#bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D7/Virtual/07-04-2022
+bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D7/Virtual/07-04-2022
 
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Presential/11-03-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D4/Virtual/24-02-2022
