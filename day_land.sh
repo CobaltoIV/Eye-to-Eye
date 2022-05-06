a5ad99796853ab062b86b4eaaae52358b1f3966e
CobaltoIV <ricas1998@gmail.com>
2022-05-06 15:06:54 +0100

    Merge

--- day_land.sh
+++ day_land.sh
@@ -17,10 +17,10 @@
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/29-03-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/07-04-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/15-03-2022
-bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/04-05-2022
+#bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/04-05-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D5/Virtual/03-03-2022
-#bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D7/Virtual/21-04-2022
-#bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D7/Virtual/14-04-2022
+#bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D7/Virtual/05-05-2022
+bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D4/Virtual/05-05-2022
 
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Presential/11-03-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D4/Virtual/24-02-2022
