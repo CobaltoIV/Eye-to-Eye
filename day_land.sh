c6cf1a49b13815de0f0a4620d0d7a0ef375bebb4
CobaltoIV <ricas1998@gmail.com>
2022-05-12 17:31:03 +0100

    Finalize D4 gaze analysis

--- day_land.sh
+++ day_land.sh
@@ -19,8 +19,8 @@
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/15-03-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Virtual/04-05-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D5/Virtual/03-03-2022
-#bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D7/Virtual/05-05-2022
-bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D4/Virtual/05-05-2022
+bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D7/Virtual/12-05-2022
+bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D4/Virtual/12-05-2022
 
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D16/Presential/11-03-2022
 #bash landmarking_script.sh -d /media/vislab/My_Passport/Tese/videos/MedGeral/D4/Virtual/24-02-2022
